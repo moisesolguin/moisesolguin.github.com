d6efac907c5e0d5c2af812814f42707a361a4053
moisesolguin <42701393+moisesolguin@users.noreply.github.com>
2018-09-21 22:00:23 -0300

    ahorasi

---
+++ ranking/assets/scripts/main.js
@@ -0,0 +1,26 @@
+// alert("bienvenido");
+let Elementlist = document.querySelectorAll(".fa-thumbs-up");
+
+Elementlist.forEach(function(element){
+    element.addEventListener('click',function(){
+        // alert("like");
+        element.classList.remove("far");
+        element.classList.add("fas");
+    })
+})
+let closes = document.querySelectorAll(".close");
+let content = document.querySelector(".content");
+
+closes.forEach(function(close){
+    close.addEventListener('click',function(ev){
+        ev.preventDefault();
+        content.classList.remove("animated");
+        content.classList.remove("bounceInRight");
+        content.classList.add("animated");
+        content.classList.add("bounceOutRight");
+        setTimeout(() => {
+            location.href="/index.html";
+        },500);
+
+    })
+})
